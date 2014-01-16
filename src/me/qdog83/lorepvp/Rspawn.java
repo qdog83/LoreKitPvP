dfc35f4d447274871644b176c257c6f6ac412ef6
blackicetf2 <blackicetf2@hotmail.com>
2014-01-16 17:45:09 -0500

    Update Rspawn.java
    Import the right shit cause I didnt used eclipse #yolo

--- src/me/qdog83/lorepvp/Rspawn.java
+++ src/me/qdog83/lorepvp/Rspawn.java
@@ -1,5 +1,25 @@
 package me.qdog83.lorepvp;
 
-public class Rspawn {
+import org.bukkit.Bukkit;
+import org.bukkit
 
-}
+
+public void onDeath(EntityDamageEvent event) {
+        Entity player = event.getEntity();
+        if (player instanceof Player) {
+            Player p = (Player) player;
+            Integer damage = event.getDamage();
+            Integer pHealth = p.getHealth();
+            if (pHealth - damage <= 0) {
+                event.setCancelled(true);
+                Location loc = new Location(player.getWorld(), 1497.40776, 75,
+                        -316.91667);
+                p.teleport(loc);
+                p.getInventory().clear();
+                p.setHealth(10);
+                p.sendMessage(ChatColor.RED + "You have died.");
+                p.setFoodLevel(10);
+            }
+ 
+        }
+    }
